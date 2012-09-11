29dbe06188fefe5cacbe835eeefe64d6804f35ee
Wan Muzaffar <wanmuz@rocketmail.com>
2012-09-11 18:34:27 +0200

    Generated the Rails 3 Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lawak::Application.config.secret_token = '9dee1fd55e1706841201bc2394fc091cbf7cf2016a75cff80558efd2f920d78ee67d9b009b9cdafbb9ee4cd72d52bb748ecab067d1ca489e3eb054b3f259270b'
