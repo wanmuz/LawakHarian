0e26e8e9bee53ab2e2321dc1f36af27f04bf7fd1
Wan Muzaffar <wanmuz@rocketmail.com>
2012-09-14 17:27:48 +0200

    Users should be redirected back to joke page if they try going to a fact that doesn't exist.

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,4 +1,5 @@
 class JokesController < ApplicationController
+  before_filter :find_joke, :only => [:show, :edit, :update, :destroy]
   def index
     @jokes = Joke.all
   end
@@ -36,5 +37,12 @@ class JokesController < ApplicationController
     @joke.destroy
     flash[:notice] = "Joke has been deleted."
     redirect_to jokes_path
+  end
+  private
+  def find_joke
+@joke = Joke.find(params[:id])
+rescue ActiveRecord::RecordNotFound
+  flash[:alert] =   "The joke you were looking for could not be found."
+redirect_to jokes_path 
   end
 end
--- spec/controllers/jokes_controller_spec.rb
+++ spec/controllers/jokes_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe JokesController do
-
+it "displays an error for a missing joke" do
+get :show, :id => "not-here"
+response.should redirect_to(jokes_path)
+message = "The joke you were looking for could not be found."
+flash[:alert].should eql(message)
+end
 end
